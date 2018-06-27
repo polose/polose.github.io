263f5a23d937337ea8f0693b0446a9d56033f2b0
Vinu <polosege@Gmail.com>
2018-06-27 12:54:32 -0700

    22

--- Login/Code-Notepad-1-master/Insidewebsite3/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-session_destroy();
-echo "You are being Logged Out";
-header("Refresh:2; url:http://localhost/login+registerfinal3/loginpageyoutube.php");
-
-
-?>
--- Login/Code-Notepad-1-master/README.md
+++
@@ -1,2 +0,0 @@
-# DELTA_TASK_3_NORMAL_MODE_LOGIN-REGISTER
-This Repository contains the HTML and CSS pages for login and sign up of Users
